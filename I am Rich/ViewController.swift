028a71392840bad68ef140e70c8bb0ce64f74b84
Шлык Дмитрий Юрьевич <Shlyk.D.Y@sberbank.ru>
2021-10-06 18:40:11 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 17876371 on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
